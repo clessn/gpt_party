a08c9cbeffd8a12c04cda2c4b90ae525971654bf
capel201 <cam.pell@me.com>
2023-11-17 15:52:09 -0500

    es ou la variable idéologie?

---
+++ code/prompting_experts.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+library(openai)
+
+data_party <- read.csv("_SharedFolder_article_spsa2024_gpt_party/data/expert_survey/dataverse_files/Global Party Survey by Party SPSS V2_1_Apr_2020-2.csv")
